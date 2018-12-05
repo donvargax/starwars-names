dac80a279fc54a77ec5fc858347d878a56b5eeb6
Jorge Vargas <yorch.veintemil+github@gmail.com>
2018-12-04 16:15:08 -0800

    chore(build): fail commits and builds when coverage below treshold

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
+
 var getRandomItem = uniqueRandomArray(starWarsNames);
 
 module.exports = {
